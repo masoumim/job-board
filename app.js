562d68bce0cf1fd1aae631d9ddce4678b38d4536
Mark Masoumi <masoumi.mark@outlook.com>
2024-07-28 20:08:25 -0400

    chore: Add home route to app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 // Parse JSON bodies
 app.use(express.json());
 
+// Home Route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 // start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
